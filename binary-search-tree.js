e43ef75d08ccb5c9cdc4ef93c384bd83cfb547ac
Brian-15 <bcirieco@gmail.com>
2022-02-01 18:54:53 -0800

    implemented insert, insertRecursively, find, findRecursively, dfsPreOrder, dfsInOrder, dfsPostOrder, bfs

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -15,56 +15,142 @@ class BinarySearchTree {
    * Returns the tree. Uses iteration. */
 
   insert(val) {
-
+    const newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let node = this.root;
+    while (true) {
+      if (val < node.val) {
+        if (!node.left) {
+          node.left = newNode;
+          break;
+        }
+        node = node.left;
+      }
+      else {
+        if (!node.right) {
+          node.right = newNode;
+          break;
+        }
+        node = node.right;
+      }
+    }
+    return this;
   }
 
   /** insertRecursively(val): insert a new node into the BST with value val.
    * Returns the tree. Uses recursion. */
 
-  insertRecursively(val) {
+  insertRecursively(val, parent=this.root) {
+    if (!this.root) {
+      this.root = new Node(val);
+      return this;
+    }
 
+    if (val < parent.val) {
+      if (!parent.left) {
+        parent.left = new Node(val);
+        return this;
+      }
+      return this.insertRecursively(val, parent.left);
+    }
+    else {
+      if (!parent.right) {
+        parent.right = new Node(val);
+        return this;
+      }
+      return this.insertRecursively(val, parent.right);
+    }
   }
 
   /** find(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses iteration. */
 
   find(val) {
+    if (!this.root) return undefined;
+    let node = this.root;
 
+    while (node != null) {
+      if (node.val === val) return node;
+      if (val < node.val) {
+        node = node.left;
+      } else {
+        node = node.right;
+      }
+    }
   }
 
   /** findRecursively(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses recursion. */
 
-  findRecursively(val) {
+  findRecursively(val, node=this.root) {
+    if (!node) return undefined;
 
+    if (node.val === val) return node;
+    
+    node = val < node.val ? node.left : node.right;
+
+    return this.findRecursively(val, node);
   }
 
   /** dfsPreOrder(): Traverse the array using pre-order DFS.
    * Return an array of visited nodes. */
 
-  dfsPreOrder() {
+  dfsPreOrder(node=this.root, visited=[]) {
+    if (!node) return visited;
+    visited.push(node.val);
+    
+    if (node.left) visited.push(...this.dfsPreOrder(node.left));
+    if (node.right) visited.push(...this.dfsPreOrder(node.right));
 
+    return visited;
   }
 
   /** dfsInOrder(): Traverse the array using in-order DFS.
    * Return an array of visited nodes. */
 
-  dfsInOrder() {
+  dfsInOrder(node=this.root, visited=[]) {
+    if (!node) return visited;
+    
+    if (node.left) visited.push(...this.dfsInOrder(node.left));
+    visited.push(node.val);
+    if (node.right) visited.push(...this.dfsInOrder(node.right));
 
+    return visited;
   }
 
   /** dfsPostOrder(): Traverse the array using post-order DFS.
    * Return an array of visited nodes. */
 
-  dfsPostOrder() {
+  dfsPostOrder(node=this.root, visited=[]) {
+    if (!node) return visited;
+    
+    if (node.left) visited.push(...this.dfsPostOrder(node.left));
+    if (node.right) visited.push(...this.dfsPostOrder(node.right));
+    visited.push(node.val);
 
+    return visited;
   }
 
   /** bfs(): Traverse the array using BFS.
    * Return an array of visited nodes. */
 
   bfs() {
+    if (!this.root) return [];
 
+    const visited = [];
+    let node = this.root;
+    const currentNodes = [this.root];
+
+    while (currentNodes.length > 0) {
+      node = currentNodes.shift();
+      visited.push(node.val);
+      if (node.left) currentNodes.push(node.left);
+      if (node.right) currentNodes.push(node.right);
+    }
+    return visited;
   }
 
   /** Further Study!
@@ -78,7 +164,7 @@ class BinarySearchTree {
   /** Further Study!
    * isBalanced(): Returns true if the BST is balanced, false otherwise. */
 
-  isBalanced() {
+  isBalanced(node, leftN=0, rightN=0) {
 
   }
 
